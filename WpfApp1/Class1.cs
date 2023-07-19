10a10343dbacc2ce5fd7d5d601e2745004492e1a
Админ <@LAPTOP-66VQ0Q0M>
2023-07-19 10:19:54 +0700

    Добавьте файлы проекта.

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    class PieChart
+    {
+        private readonly double _factor = 0.666666666666667;
+        protected readonly double PaddingChart = 10;
+        public double WidthChart;
+        public double HeightChart;
+
+        public Canvas ChartBackground = new Canvas();
+
+        public PieChart()
+        {
+            ChartBackground.Margin = new Thickness(0);
+            ChartBackground.SizeChanged += ChartBackground_SizeChanged;
+        }
+
+        private void ChartBackground_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Инициализация ширины и высоты графиков.
+            WidthChart = e.NewSize.Width - (PaddingChart * 2);
+
+            // Верхний предел графика на 10 линии снизу.
+            HeightChart = e.NewSize.Height * _factor;
+        }
+
+        public void AddValue(double value)
+        {
+            List<StoredValues> listValues = СalculateSectorAngle(value);
+
+            // Удалим все элементы перед созданием новых актуальных.
+            Clear();
+
+            // Размещение секторов вычисленного размера для создания Pie Chart.
+            for (int i = 0; i < listValues.Count; i++)
+            {
+                StoredValues sv = listValues[i];
+
+                // Каждый Path-элемент будет хранить данные сектора для последующих вычислений.
+                Path p = CreateSector(sv.Degree, sv.Offset, sv.Value, i);
+                _ = ChartBackground.Children.Add(p);
+
+                // Числовые значения секторов диска.
+                Label label = new Label()
+                {
+                    Content = sv.Value
+                };
+
+                // Цветовые метки перед числовыми
+                Rectangle r = new Rectangle()
+                {
+                    Width = 16,
+                    Height = 12,
+                    Fill = p.Fill,
+                    Stroke = Brushes.White,
+                    StrokeThickness = 1
+                };
+
+                StackPanel sp = new StackPanel()
+                {
+                    Orientation = Orientation.Horizontal
+                };
+                sp.Children.Add(r);
+                sp.Children.Add(label);
+                Canvas.SetLeft(sp, 10);
+                Canvas.SetTop(sp, 20 * i);
+                _ = ChartBackground.Children.Add(sp);
+            }
+        }
+
+
+        public void Clear()
+        {
+            ChartBackground.Children.Clear();
+        }
+
+
+        private Path CreateSector(double degree, double offset, double value, int k)
+        {
+            SolidColorBrush Fl = new SolidColorBrush(Color.FromRgb(0,0,0));
+            if (k == 1)
+            {
+                Fl = new SolidColorBrush(Color.FromRgb(255, 128, 0));
+            }
+            if (k == 0)
+            {
+                Fl = new SolidColorBrush(Color.FromRgb(0,255, 0));
+            }
+            if (k == 2)
+            {
+                Fl = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            }
+
+
+            Path path = new Path()
+            {
+                StrokeThickness = 5,
+                Stroke = Brushes.White,
+                Fill = Fl,
+
+                Data = new PathGeometry()
+                {
+                    Figures = new PathFigureCollection()
+                    {
+                        SectorGeometry(degree, offset)
+                    }
+                },
+
+                Tag = new StoredValues()
+                {
+                    Degree = degree,
+                    Offset = offset,
+                    Value = value
+                }
+            };
+            return path;
+        }
+
+
+        public PathFigure SectorGeometry(double degree, double offset)
+        {
+            double _radius = 100;
+            bool islarge = false;
+            if (degree > 180) islarge = true;
+            if (degree >= 360) degree = 359.999;
+            Point centerPoint = new Point(ChartBackground.ActualWidth / 2, ChartBackground.ActualHeight / 2);
+            Point startPoint = new Point(centerPoint.X, centerPoint.Y + _radius);
+            Point endPoint = startPoint;
+            RotateTransform rotateStartPoint = new RotateTransform(offset)
+            {
+                CenterX = centerPoint.X,
+                CenterY = centerPoint.Y
+            };
+            startPoint = rotateStartPoint.Transform(startPoint);
+            RotateTransform rotateEndPoint = new RotateTransform(offset + degree)
+            {
+                CenterX = centerPoint.X,
+                CenterY = centerPoint.Y
+            };
+            endPoint = rotateEndPoint.Transform(endPoint);
+            PathFigure sector = new PathFigure()
+            {
+                StartPoint = startPoint,
+                Segments = new PathSegmentCollection()
+                {
+                    new ArcSegment()
+                    {
+                        Point = endPoint,
+                        Size = new Size(_radius, _radius),
+                        SweepDirection = SweepDirection.Clockwise,
+                        IsLargeArc = islarge,
+                        IsStroked = true
+                    },
+
+                    new PolyLineSegment()
+                    {
+                        Points = new PointCollection() { endPoint, centerPoint, startPoint },
+                        IsStroked = false
+                    }
+                }
+            };
+
+            return sector;
+        }
+
+        private List<StoredValues> СalculateSectorAngle(double value)
+        {
+            List<StoredValues> listValues = ChartBackground.Children.OfType<Path>().Select(p => (StoredValues)p.Tag).ToList();
+            StoredValues d = new StoredValues();
+            d.Value = value;
+            listValues.Add(d);
+
+            double sum = listValues.Select(p => p.Value).Sum();
+            double denominator = sum / 360;
+            for (int i = 0; i < listValues.Count; i++)
+            {
+                double degree = Math.Round(listValues[i].Value / denominator, 2);
+                listValues[i].Degree = degree;
+                double offset = 0;
+                if (i > 0)
+                {
+                    offset = listValues[i - 1].Degree + listValues[i - 1].Offset;
+                }
+
+                listValues[i].Offset = offset;
+            }
+
+            return listValues;
+        }
+
+        internal class StoredValues
+        {
+            public double Degree;
+            public double Offset;
+            public double Value;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Json;
+using System.Globalization;
+
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+
+    public partial class MainWindow : Window
+    {
+        public List<string> address = new List<string>();
+        public ExName[] rep = new ExName[0];
+        public Table[] Trep = new Table[0];
+        public List<Table> ListEx = new List<Table>();
+        public MainWindow()
+        {
+            //маштабирование по всему экрану
+            double width = System.Windows.SystemParameters.PrimaryScreenWidth+22;
+            double height = System.Windows.SystemParameters.PrimaryScreenHeight+10;
+            InitializeComponent();
+            var crd = System.Windows.SystemParameters.WorkArea.TopLeft;
+            this.Left = crd.X-10;
+            this.Top = crd.Y-1;
+            HostingWfInWpf.Width = width;
+            HostingWfInWpf.Height = height;
+            //создание переменных
+
+            //заполнение адресов
+            address.Add("https://localhost:7204");
+            address.Add("");
+            address.Add("");
+            address.Add("");
+
+
+            rep =new ExName[address.Count];
+            Trep = new Table[address.Count];
+            //заполнение данных и заполнение листа
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+
+        }
+
+        //вспомогательный класс для вывода в DataGrid
+        public class Table
+        {
+            public string name { get; set; }
+            public string col { set; get; }
+            public string Exep { get; set; }          
+            public Table(string Name = "", string ex = "", string ExeptEx = "")
+            {
+                this.name = Name;
+                this.Exep = ExeptEx;
+                this.col = ex;
+            }
+        }
+
+        //Основной класс для заполнения
+        public class ExName
+        {
+            public string Name 
+            { get; set; }
+            public string ex { set; get; }
+            public bool IfExe { get; set; }
+            public string ExeptEx { get; set; }
+            public ExName(string Name = "", string ex = "", bool IfExe = true, string ExeptEx = "")
+            {
+                this.Name = Name;
+                switch (ex)
+                {
+                    case "Error":
+                        this.ex = ex; break;
+                    case "DataError":
+                        this.ex = ex; break;
+                    case "Ok":
+                        this.ex = ex;break;
+                    default: this.ex = "not Information"; break;
+                }
+                this.IfExe = IfExe;
+                this.ExeptEx = ExeptEx;
+            }
+
+            public Table CreateTable()
+            {
+                return new Table(Name, ex, ExeptEx);
+            }
+        }
+
+        //медот отправки запрос и возвращения данных
+        public async Task<ExName> GetClass(string address)
+        {
+            HttpClient client = new HttpClient();
+            ExName rep = new ExName();
+            try
+            {
+                rep = await client.GetFromJsonAsync<ExName>(address);
+                if (rep != null)
+                {
+                    return rep;
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ExName("None", "Error", false, "");
+            }
+            return new ExName("None", "Error", false, "");
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            ListEx.Clear();
+            for (int i = 0; i < address.Count; i++)
+            {
+                Task<ExName> serp = Task.Run(() => GetClass(address[i]));
+                rep[i] = serp.Result;
+                Trep[i] = rep[i].CreateTable();
+                ListEx.Add(Trep[i]);
+            }
+            //вывод данных  в DataGrid
+
+            Dgrid.ItemsSource = ListEx;
+            GrChart.Children.OfType<Canvas>().ToList().ForEach(p => GrChart.Children.Remove(p));
+            PieChart chart = null;
+            chart = new PieChart();
+            GrChart.Children.Add(chart.ChartBackground);
+            GrChart.UpdateLayout();
+
+            CreateChart(chart);
+            Dgrid.Items.Refresh();
+        }
+
+        private void CreateChart(PieChart chart)
+        {
+            chart.Clear();
+
+            for (int i = 0; i < 3; i++)
+            {
+                chart.AddValue(Counti(i));
+            }
+        }
+
+        private int Counti(int i)
+        {
+            int count = 0;
+            if (i==2)
+            {
+                foreach(var x in rep)
+                {
+                    if (x.ex=="Error")
+                        count++;
+                }
+            }
+            if (i == 1)
+            {
+                foreach (var x in rep)
+                {
+                    if (x.ex == "DataError")
+                        count++;
+                }
+            }
+            if (i == 0)
+            {
+                foreach (var x in rep)
+                {
+                    if (x.ex == "Ok")
+                        count++;
+                }
+            }
+            return count;
+        }
+    }
+}
